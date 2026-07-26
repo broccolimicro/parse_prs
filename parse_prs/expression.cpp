fab36efc1c9ee178819a14e31a67aed737dfbb0a
Ned Bingham <edward.bingham@broccolimicro.io>
2026-07-26 07:03:42 -0400

    using set_lvalue

--- parse_prs/expression.cpp
+++ parse_prs/expression.cpp
@@ -43,6 +43,8 @@ parse_expression::config makeExprConfig() {
 	cfg.order.push(operation_set::UNARY);
 	cfg.order.push_back("~", "", "", "");
 
+	cfg.set_lvalue();
+
 	cfg.order.push(operation_set::MODIFIER);
 	cfg.order.push_back("", "'", "", "");
 
@@ -53,8 +55,6 @@ parse_expression::config makeExprConfig() {
 	
 	cfg.order.push(operation_set::MODIFIER);
 	cfg.order.push_back("", "::", "", "");
-
-	cfg.lvalueLevel = cfg.order.size()-3;
 
 	return cfg;
 }
@@ -75,7 +75,7 @@ parse_expression::config makeCompConfig() {
 	cfg.order.push(operation_set::BINARY);
 	cfg.order.push_back("", "", ",", "");
 
-	cfg.lvalueLevel = 2;
+	cfg.set_lvalue();
 
 	return cfg;
 }
